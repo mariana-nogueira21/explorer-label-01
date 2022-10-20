7d4e49f56105b4acd110ca010dc213c517255469
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2022-10-20 20:43:48 -0300

    finalização do projeto rocketpay

--- src/main.js
+++ src/main.js
@@ -66,11 +66,55 @@ const cardNumberPattern = {
     const foundMask = dynamicMasked.compiledMasks.find(function(item) {
       return number.match(item.regex)
     })
-    console.log(foundMask)
+    //console.log(foundMask)
 
     return foundMask
   },
 }
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
 
-cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado!")
+})
 
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+
+  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecutiry = document.querySelector(".cc-security .value")
+
+  ccSecutiry.innerText = code.length === 0 ? "123" : code
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+expirationDateMasked.on("accept", () => {
+  updateExprirationDate(expirationDateMasked.value)
+})
+
+function updateExprirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value")
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
